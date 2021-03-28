00a02d73cd863802941277d595da8b5201108558
Felipe Gomez <fgomezcode@gmail.com>
2021-03-27 17:34:32 -0700

    Started working on level building - procGen

---
+++ Dungeon_Dudes/Assets/LevelSpawnerTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawnerTest : MonoBehaviour
+{
+   [Header("Wall Location")]
+    public bool topLeft = false;
+    public bool topMid = false;
+    public bool topRight = false;
+}
+
